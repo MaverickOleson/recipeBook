b19970f33843136ea6e2725172c9b6eac24cbff8
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-04-18 15:26:54 -0700

    built database and am testing fetch

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Book({ pages }) {
 	var page = 0;
@@ -24,15 +24,28 @@ export default function Book({ pages }) {
 				return e.target.previousElementSibling.style.display = 'none'
 			}
 			pageSwitch = true;
-			e.target.style.zIndex = 1;
+			e.target.addEventListener('animationend', animEnd);
+			function animEnd(ev) {
+				if (ev.animationName === 'halfTurnPage') {
+					e.target.style.zIndex = 1;
+					e.target.removeEventListener('animationend', animEnd);
+				}
+			}
 			return e.target.classList.add('turnPage');
 		}
 		if (page === 2) {
-			console.log(page)
+			console.log(page, '2')
 			page--;
 			if (pageSwitch) {
 				pageSwitch = false;
-				return e.target.style.display = 'none'
+				e.target.addEventListener('animationend', animEnd);
+				function animEnd(ev) {
+					if (ev.animationName === 'halfTurnPage') {
+						e.target.style.zIndex = 2;
+						e.target.removeEventListener('animationend', animEnd);
+					}
+				}
+				return e.target.classList.add('turnPage');
 			}
 			pageSwitch = true;
 			return e.target.style.display = 'none'
@@ -42,5 +55,6 @@ export default function Book({ pages }) {
 		<div className="page" onClick={e => flipBook(e)}>Cover</div>
 		<div className="page" onClick={e => flipBook(e)}>Page1</div>
 		<div className="page" onClick={e => flipBook(e)}>Page2</div>
+		<div className="page" onClick={e => flipBook(e)}>Page3</div>
 	</div>
 }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Book from '../components/Book';
+import axios from 'axios';
 
 export default function Recipes() {
     document.getElementById('root').className = 'recipes centerScreen';
+    const [recipes, setRecipes] = useState();
+    useEffect(async () => {
+        setRecipes((await axios.get('https://myrecipesapi.herokuapp.com/')).data);
+        console.log((await axios.get('https://myrecipesapi.herokuapp.com/')).data.cookie.ingredients)
+    }, []);
     return (
-        <Book />
+        <>
+            <Book />
+            {(recipes.cookie.ingredients) ? recipes.cookie.ingredients : 's'}
+        </>
     )
 }
