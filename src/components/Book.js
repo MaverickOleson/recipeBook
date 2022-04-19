d4052b53e91324789c674624d86e69935fb362d3
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-04-19 15:18:33 -0700

    api info fetched, turning pages works better

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,60 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function Book({ pages }) {
-	var page = 0;
-	var pageSwitch = false;
-	function flipBook(e) {
-		if (!page) {
-			page++;
-			e.target.parentElement.classList.add('openBook');
-			e.target.addEventListener('animationend', animEnd);
-			function animEnd(ev) {
-				if (ev.animationName === 'halfTurnPage') {
-					e.target.style.zIndex = 0;
-					e.target.removeEventListener('animationend', animEnd);
-				}
+	const [pageIndex, setPageIndex] = useState(0);
+	var flip = true;
+
+	if (pages) return <div className='book'>
+		<div className="page" onClick={e => {
+			if (flip) {
+				e.target.classList.add('turnPage');
+				e.target.parentElement.classList.add('openBook');
+				e.target.classList.remove('turnPageBack');
+				return flip = false;
 			}
-			return e.target.classList.add('turnPage');
-		}
-		if (page === 1) {
-			page++;
-			console.log(page)
-			if (pageSwitch) {
-				pageSwitch = false;
-				return e.target.previousElementSibling.style.display = 'none'
+			e.target.classList.add('turnPageBack');
+			e.target.parentElement.classList.remove('openBook');
+			e.target.classList.remove('turnPage');
+			return flip = true;
+		}}>Cover</div>
+		<div className="page" onClick={e => {
+			if (!flip) {
+				e.target.classList.add('turnPage');
+				e.target.classList.remove('turnPageBack');
+				return flip = true;
 			}
-			pageSwitch = true;
-			e.target.addEventListener('animationend', animEnd);
-			function animEnd(ev) {
-				if (ev.animationName === 'halfTurnPage') {
-					e.target.style.zIndex = 1;
-					e.target.removeEventListener('animationend', animEnd);
-				}
+			e.target.classList.add('turnPageBack');
+			e.target.classList.remove('turnPage');
+			return flip = false;
+		}}>{pages[pageIndex] || 'Page 1'}</div>
+		<div className="page" onClick={e => {
+			if (flip) {
+				e.target.classList.add('turnPage');
+				e.target.classList.remove('turnPageBack');
+				return flip = false;
 			}
-			return e.target.classList.add('turnPage');
-		}
-		if (page === 2) {
-			console.log(page, '2')
-			page--;
-			if (pageSwitch) {
-				pageSwitch = false;
-				e.target.addEventListener('animationend', animEnd);
-				function animEnd(ev) {
-					if (ev.animationName === 'halfTurnPage') {
-						e.target.style.zIndex = 2;
-						e.target.removeEventListener('animationend', animEnd);
-					}
-				}
-				return e.target.classList.add('turnPage');
-			}
-			pageSwitch = true;
-			return e.target.style.display = 'none'
-		}
-	}
-	return <div className='book'>
-		<div className="page" onClick={e => flipBook(e)}>Cover</div>
-		<div className="page" onClick={e => flipBook(e)}>Page1</div>
-		<div className="page" onClick={e => flipBook(e)}>Page2</div>
-		<div className="page" onClick={e => flipBook(e)}>Page3</div>
+			e.target.classList.add('turnPageBack');
+			e.target.classList.remove('turnPage');
+			return flip = true;
+		}}>{pages[pageIndex + 1] || 'Page 2'}</div>
 	</div>
 }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -5,14 +5,27 @@ import axios from 'axios';
 export default function Recipes() {
     document.getElementById('root').className = 'recipes centerScreen';
     const [recipes, setRecipes] = useState();
-    useEffect(async () => {
-        setRecipes((await axios.get('https://myrecipesapi.herokuapp.com/')).data);
-        console.log((await axios.get('https://myrecipesapi.herokuapp.com/')).data.cookie.ingredients)
+    async function getRecipes() {
+        const recipePages = [];
+        const recipes = (await axios.get('https://myrecipesapi.herokuapp.com/')).data;
+        for (const recipe in recipes) {
+            recipePages.push(
+                <>
+                    <h1>{recipe}</h1>
+                    <ol>
+                        {recipes[recipe].ingredients.map((step, index) => { return <li key={index}>{step}</li> })}
+                    </ol>
+                </>
+            );
+        }
+        setRecipes(recipePages);
+    }
+    useEffect(() => {
+        getRecipes();
     }, []);
     return (
         <>
-            <Book />
-            {(recipes.cookie.ingredients) ? recipes.cookie.ingredients : 's'}
+            <Book pages={recipes} />
         </>
     )
 }
